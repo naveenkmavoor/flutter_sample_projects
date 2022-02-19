778a5ff5006bb7db738c7d9a18b7f598a1c5972c
Naveenkmavoor <naveenkmavoor@gmail.com>
2022-02-19 18:42:45 +0530

    feat: javascript channel communication

--- sample_webview/lib/webviewscreen.dart
+++ sample_webview/lib/webviewscreen.dart
@@ -12,7 +12,7 @@ class Homepage extends StatefulWidget {
 
 class HomepageState extends State<Homepage> {
   late WebViewController _controller;
-
+  late String? _link;
   final Completer<WebViewController> _controllerCompleter =
       Completer<WebViewController>();
 
@@ -29,15 +29,73 @@ class HomepageState extends State<Homepage> {
       child: WillPopScope(
         onWillPop: () => _goBack(context),
         child: WebView(
-          onProgress: (progress) => Center(child: CircularProgressIndicator()),
           initialUrl: 'https://mbp-beta.herokuapp.com/',
+          // initialUrl: 'http://192.168.1.10:8080/app',
           javascriptMode: JavascriptMode.unrestricted,
           onWebViewCreated: (WebViewController webViewController) {
-            _controllerCompleter.future.then((value) => _controller = value);
+            _controllerCompleter.future.then((value) {
+              _controller = value;
+            });
             _controllerCompleter.complete(webViewController);
           },
+          javascriptChannels: <JavascriptChannel>{
+            // Set Javascript Channel to WebView
+            _extractDataJSChannel(context),
+          },
+          onPageStarted: (String url) {
+            print('Page started loading: $url');
+          },
+          onPageFinished: (String url) async {
+            print('Page finished loading: $url');
+            // In the final result page we check the url to make sure  it is the last page.
+            // _controller.runJavascript(
+            //     "(function(){document.getElementById('appbar').style.display='none'})();");
+            // _controller.runJavascript(
+            //     "(function(){Site.postMessage(window.document.getElementById('linkBadge'))})();");
+
+            String email = "c4@mailinator.com";
+            _controller.runJavascript(
+                "document.getElementById('TextField0').value='$email';");
+            String password = "123456";
+            _controller.runJavascript(
+                "document.getElementById('TextField3').value='$password';");
+            await Future.delayed(const Duration(seconds: 1));
+            _controller
+                .runJavascript("document.getElementById('btn-form').click();");
+          },
         ),
+
+        //  WebView(
+        //   zoomEnabled: false,
+        //   onPageFinished: (url) {
+        //     _controller.runJavascript(
+        //         "(function(){document.getElementByTagName('header')[0].style.display='none'})();");
+        // "document.getElementByTagName('header')[0].style.display='none';");
+        //   },
+        //   onPageStarted: (url) {
+        //     print('Page started loading: $url');
+        //   },
+        //   onProgress: (progress) =>
+        //       const Center(child: CircularProgressIndicator()),
+        //   initialUrl: 'https://mbp-beta.herokuapp.com/',
+        //   javascriptMode: JavascriptMode.unrestricted,
+        //   onWebViewCreated: (WebViewController webViewController) {
+        //     _controllerCompleter.future.then((value) => _controller = value);
+        //     _controllerCompleter.complete(webViewController);
+        //   },
+        // ),
       ),
+    );
+  }
+
+  JavascriptChannel _extractDataJSChannel(BuildContext context) {
+    return JavascriptChannel(
+      name: 'Site',
+      onMessageReceived: (JavascriptMessage message) {
+        String pageBody = message.message;
+        _link = pageBody;
+        // print('------------------------ RESULT: $pageBody');
+      },
     );
   }
 
